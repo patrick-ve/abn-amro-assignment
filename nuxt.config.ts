fb452a72bccfd2f01a523f3fd82162c50f906146
Patrick van Everdingen <patrick@passionatepeople.io>
2025-04-04 15:37:03 +0200

    chore: upgrade nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: '2025-04-04',
 
   devtools: { enabled: true },
+
+  future: {
+    compatibilityVersion: 4,
+  },
 })
