b558171ff98a53acd39f0c88c9741e7ef2266bf1
Patrick van Everdingen <patrick@passionatepeople.io>
2025-04-04 15:49:18 +0200

    feat: install tailwindcss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-04-04',
-
   devtools: { enabled: true },
 
   future: {
     compatibilityVersion: 4,
   },
+
+  modules: ['@nuxtjs/tailwindcss'],
 })
