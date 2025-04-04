c1ee8e7bb7fbe98b22c66fe53b406f393bba29f1
Patrick van Everdingen <patrick@passionatepeople.io>
2025-04-04 16:56:09 +0200

    feat: update type

--- app/types/show.ts
+++ app/types/show.ts
@@ -1,24 +1,59 @@
 export interface Show {
   id: number
+  url: string
   name: string
+  type: string
+  language: string
   genres: string[]
+  status: string
+  runtime: number
+  averageRuntime: number
+  premiered: string | null
+  ended: string | null
+  officialSite: string | null
+  schedule: {
+    time: string
+    days: string[]
+  }
   rating: {
     average: number | null
+  }
+  weight: number
+  network?: {
+    id: number
+    name: string
+    country: {
+      name: string
+      code: string
+      timezone: string
+    }
+    officialSite: string
+  }
+  webChannel: null | {
+    id: number
+    name: string
+    officialSite: string | null
+  }
+  dvdCountry: string | null
+  externals: {
+    tvrage: number | null
+    thetvdb: number | null
+    imdb: string | null
   }
   image: {
     medium: string
     original: string
   } | null
   summary: string | null
-  premiered: string | null
-  ended: string | null
-  status: string
-  network?: {
-    name: string
-  }
-  schedule: {
-    time: string
-    days: string[]
+  updated: number
+  _links: {
+    self: {
+      href: string
+    }
+    previousepisode?: {
+      href: string
+      name: string
+    }
   }
 }
 
