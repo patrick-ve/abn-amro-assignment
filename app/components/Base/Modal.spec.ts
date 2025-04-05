30171d4c0567489698e9067e7ea281107e6f08f5
Patrick van Everdingen <patrick@passionatepeople.io>
2025-04-05 20:58:03 +0200

    chore: remove unused import

--- app/components/Base/Modal.spec.ts
+++ app/components/Base/Modal.spec.ts
@@ -1,5 +1,5 @@
 import { fireEvent, render, screen } from '@testing-library/vue'
-import { describe, expect, it, vi } from 'vitest'
+import { describe, expect, it } from 'vitest'
 import Modal from './Modal.vue'
 
 describe('modal', () => {
