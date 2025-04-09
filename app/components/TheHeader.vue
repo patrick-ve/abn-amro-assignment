bc83f28ceb3bfc84813476f76d0523c29083ddc3
Patrick van Everdingen <patrick@passionatepeople.io>
2025-04-09 17:02:22 +0200

    feat: improve header ui

--- app/components/TheHeader.vue
+++ app/components/TheHeader.vue
@@ -1,17 +1,19 @@
 <template>
-  <header class="fixed top-0 left-0 right-0 z-50 flex justify-between items-center h-20 bg-black/50 backdrop-blur-md px-6 container mx-auto py-8">
-    <div class="flex-1">
-      <slot name="left" />
-    </div>
+  <div class="fixed top-0 left-0 right-0 z-50 h-20 bg-black/50 backdrop-blur-md">
+    <header class="flex justify-between items-center h-full px-6 container mx-auto py-8">
+      <div class="flex-1">
+        <slot name="left" />
+      </div>
 
-    <div class="flex-1 flex justify-center">
-      <slot name="center">
-        <img src="/abnflix_logo.png" alt="ABNFlix Logo" class="h-32">
-      </slot>
-    </div>
+      <div class="flex-1 flex justify-center">
+        <slot name="center">
+          <img src="/abnflix_logo.png" alt="ABNFlix Logo" class="h-32">
+        </slot>
+      </div>
 
-    <div class="flex-1 flex justify-end">
-      <slot name="right" />
-    </div>
-  </header>
+      <div class="flex-1 flex justify-end">
+        <slot name="right" />
+      </div>
+    </header>
+  </div>
 </template>
