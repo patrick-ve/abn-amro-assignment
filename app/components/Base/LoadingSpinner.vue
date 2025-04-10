b9d34a6b86b868e56bf6cce4e8316c3eda62b6d2
Patrick van Everdingen <patrick@passionatepeople.io>
2025-04-10 13:12:42 +0200

    feat: change loading spinner ui

--- app/components/Base/LoadingSpinner.vue
+++ app/components/Base/LoadingSpinner.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="flex justify-center items-center min-h-[400px]">
-    <div class="w-12 h-12 border-4 border-gray-300 rounded-full animate-spin border-t-blue-600" />
+    <div class="w-12 h-12 border-4 border-gray-300 rounded-full animate-spin border-t-red-500" />
   </div>
 </template>
