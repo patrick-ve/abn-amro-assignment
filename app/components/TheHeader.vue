9ee22a5c61e2732faaf7f9de62c580e056a69fb3
Patrick van Everdingen <patrick@passionatepeople.io>
2025-04-10 11:11:57 +0200

    chore: sort tailwind classes

--- app/components/TheHeader.vue
+++ app/components/TheHeader.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="fixed top-0 left-0 right-0 z-50 h-20 bg-black/50 backdrop-blur-md">
-    <header class="flex justify-between items-center h-full px-6 container mx-auto py-8">
+    <header class="container flex items-center justify-between h-full px-6 py-8 mx-auto">
       <div class="flex-1">
         <slot name="left" />
       </div>
 
-      <div class="flex-1 flex justify-center">
+      <div class="flex justify-center flex-1">
         <slot name="center">
           <img src="/abnflix_logo.png" alt="ABNFlix Logo" class="h-32">
         </slot>
       </div>
 
-      <div class="flex-1 flex justify-end">
+      <div class="flex justify-end flex-1">
         <slot name="right" />
       </div>
     </header>
